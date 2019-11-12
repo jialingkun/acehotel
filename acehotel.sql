f69fccab1176ea790b8d4cbc5fe020e23ebc7654
Enny Hartz Tono <djaka.tingkir2@gmail.com>
2019-11-12 15:02:12 +0700

    Update acehotel.sql

--- acehotel.sql
+++ acehotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2019 at 01:15 PM
+-- Generation Time: Nov 12, 2019 at 09:01 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 5.6.38
 
@@ -58,8 +58,7 @@ CREATE TABLE `kamar` (
   `id_hotel` varchar(255) DEFAULT NULL,
   `nama_kamar` varchar(255) DEFAULT NULL,
   `jumlah_kamar` int(11) DEFAULT NULL,
-  `max_guest` int(11) DEFAULT NULL,
-  `harga_kamar` int(11) DEFAULT NULL
+  `max_guest` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -72,20 +71,28 @@ CREATE TABLE `order` (
   `id_order` varchar(255) NOT NULL,
   `id_hotel` varchar(255) DEFAULT NULL,
   `id_kamar` varchar(255) DEFAULT NULL,
+  `username_owner` varchar(255) DEFAULT NULL,
   `nama_pemesan` varchar(255) DEFAULT NULL,
   `telepon_pemesan` varchar(255) DEFAULT NULL,
+  `email_pemesan` varchar(255) DEFAULT NULL,
+  `no_ktp_pemesan` varchar(255) DEFAULT NULL,
   `tanggal_check_in` date DEFAULT NULL,
   `tanggal_check_out` date DEFAULT NULL,
-  `status_pembayaran` varchar(255) DEFAULT NULL,
-  `status_order` varchar(255) DEFAULT NULL,
-  `sumber_order` varchar(255) DEFAULT NULL,
-  `tanggal_order` date DEFAULT NULL,
-  `request_jam_check_in` date DEFAULT NULL,
   `jumlah_guest` int(11) DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL,
-  `total_harga` int(11) DEFAULT NULL,
+  `jumlah_room` int(11) DEFAULT NULL,
+  `max_guest` int(11) DEFAULT NULL,
   `nama_kamar` varchar(255) DEFAULT NULL,
-  `max_guest` int(11) DEFAULT NULL
+  `nama_hotel` varchar(255) DEFAULT NULL,
+  `alamat_hotel` varchar(255) DEFAULT NULL,
+  `telepon_hotel` varchar(255) DEFAULT NULL,
+  `request_jam_check_in_awal` time DEFAULT NULL,
+  `request_jam_check_in_akhir` time DEFAULT NULL,
+  `request_breakfast` tinyint(1) DEFAULT NULL,
+  `request_rent_car` tinyint(1) DEFAULT NULL,
+  `total_harga` int(11) DEFAULT NULL,
+  `tanggal_order` date DEFAULT NULL,
+  `sumber_order` varchar(255) DEFAULT NULL,
+  `status_order` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -129,19 +136,24 @@ ALTER TABLE `admin`
 -- Indexes for table `hotel`
 --
 ALTER TABLE `hotel`
-  ADD PRIMARY KEY (`id_hotel`);
+  ADD PRIMARY KEY (`id_hotel`),
+  ADD KEY `hotel_ibfk_1` (`username_owner`);
 
 --
 -- Indexes for table `kamar`
 --
 ALTER TABLE `kamar`
-  ADD PRIMARY KEY (`id_kamar`);
+  ADD PRIMARY KEY (`id_kamar`),
+  ADD KEY `kamar_ibfk_1` (`id_hotel`);
 
 --
 -- Indexes for table `order`
 --
 ALTER TABLE `order`
-  ADD PRIMARY KEY (`id_order`);
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_hotel` (`id_hotel`),
+  ADD KEY `id_kamar` (`id_kamar`),
+  ADD KEY `username_owner` (`username_owner`);
 
 --
 -- Indexes for table `owner`
@@ -153,7 +165,38 @@ ALTER TABLE `owner`
 -- Indexes for table `receptionist`
 --
 ALTER TABLE `receptionist`
-  ADD PRIMARY KEY (`username_receptionist`);
+  ADD PRIMARY KEY (`username_receptionist`),
+  ADD KEY `receptionist_ibfk_1` (`id_hotel`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `hotel`
+--
+ALTER TABLE `hotel`
+  ADD CONSTRAINT `hotel_ibfk_1` FOREIGN KEY (`username_owner`) REFERENCES `owner` (`username_owner`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `kamar`
+--
+ALTER TABLE `kamar`
+  ADD CONSTRAINT `kamar_ibfk_1` FOREIGN KEY (`id_hotel`) REFERENCES `hotel` (`id_hotel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`id_hotel`) REFERENCES `hotel` (`id_hotel`),
+  ADD CONSTRAINT `order_ibfk_2` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`),
+  ADD CONSTRAINT `order_ibfk_3` FOREIGN KEY (`username_owner`) REFERENCES `owner` (`username_owner`);
+
+--
+-- Constraints for table `receptionist`
+--
+ALTER TABLE `receptionist`
+  ADD CONSTRAINT `receptionist_ibfk_1` FOREIGN KEY (`id_hotel`) REFERENCES `hotel` (`id_hotel`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
