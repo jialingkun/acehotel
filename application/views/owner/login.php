31f665eb95a3da601183730d79502e63cb1bc3f8
Yoko <40120kyoko@gmail.com>
2019-11-16 12:22:11 +0700

    bootstrap framework installation

--- application/views/owner/login.php
+++ application/views/owner/login.php
@@ -2,6 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="<?=base_url("dist/css/bootstrap.min.css");?>">
+	<link rel="stylesheet" href="<?=base_url("dist/css/bootstrap-grid.min.css");?>">
+	<script src="<?=base_url("dist/js/jquery.min.js");?>"></script>
+	<script src="<?=base_url("dist/js/bootstrap.min.js");?>"></script>
+	
 	<title>LOGIN</title>
 </head>
 <body>
