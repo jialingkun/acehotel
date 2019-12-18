958be13da4df22eaa152e1a80c5300a50e012792
Yoko <40120kyoko@gmail.com>
2019-12-18 08:57:34 +0700

    UI v3
    { MASIH ADMIN UI SAJA }
    
    1. dashboard:
    - fixed UI di dropdrown hotel
    - fixed nama hotel d dropdown yg panjang berpengaruh ke UI
    
    2. semua footer nav berfungsi
    3. header text done

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -2,16 +2,16 @@
 <nav class="navbar fixed-bottom navbar-expand navbar-light bg-light text-center no-padding" >
     <ul class="navbar-nav" style="margin:auto; width:100%;">
       <li class="nav-item " style="margin:auto; width:100%;">
-        <a class="nav-link is-active" href="#" style="padding:10px">Home</a>
+        <a class="nav-link" id="dashboard_footer" href="<?=base_url("/index.php/dashboardadmin");?>" style="padding:10px">Dashboard</a>
       </li>
       <li class="nav-item" style="margin:auto; width:100%;">
-        <a class="nav-link" href="#" style="padding:10px">Bookings</a>
+        <a class="nav-link" id="booking_footer" href="<?=base_url("/index.php/bookingadmin");?>" style="padding:10px">Bookings</a>
       </li>
       <li class="nav-item" style="margin:auto; width:100%;">
-        <a class="nav-link" href="#" style="padding:10px">Report</a>
+        <a class="nav-link" id="report_footer" href="<?=base_url("/index.php/reportadmin");?>" style="padding:10px">Report</a>
       </li>
       <li class="nav-item" style="margin:auto; width:100%;">
-        <a class="nav-link" href="#" style="padding:10px">Manage</a>
+        <a class="nav-link" id="management_footer" href="<?=base_url("/index.php/managementadmin");?>" style="padding:10px">Manage</a>
       </li>
     </ul>
 </nav>
